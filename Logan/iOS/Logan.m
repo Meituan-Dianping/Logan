bc7c17c3e96255ebdc1891f2bf8574f8b6ef33bf
jiangteng <jiangteng.cn@gmail.com>
2020-02-12 13:37:07 +0800

    fix:  unionid value in iOS upload request header

--- Logan/iOS/Logan.m
+++ Logan/iOS/Logan.m
@@ -410,7 +410,7 @@ NSString *_Nonnull loganTodaysDate(void) {
 			[req addValue:appId forHTTPHeaderField:@"appId"];
 		}
 		if(unionId.length >0){
-			[req addValue:appId forHTTPHeaderField:@"unionId"];
+			[req addValue:unionId forHTTPHeaderField:@"unionId"];
 		}
 		NSString *bundleVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
 		if (bundleVersion.length > 0) {
